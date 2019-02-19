86a788eeef8c8868d1d2c0aa605879931a506310
Sri Pravan Paturi <pravanpaturi@gmail.com>
2019-02-19 16:04:18 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+ICMLSC 2019 Website
+===================
+This repo holds the code for the ICMLSC (International Conference on Machine Learning, Security and Cloud Computing) 2019 Website.
---
+++ js/script.js
@@ -0,0 +1,19 @@
+(function ($) {
+    function startCountDown() {
+        var countDownDate = new Date("Jan 3, 2019 0:0:0").getTime();
+        setInterval(function () {
+            var now = new Date().getTime();
+            var distance = countDownDate - now;
+            if (distance > 0) {
+                var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+                var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+                var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+                var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+                $(".countdown").html(days + "d " + hours + "h " + minutes + "m " + seconds + "s");
+            }
+        }, 1000);
+    }
+    $(document).ready(function () {
+        startCountDown();
+    });
+})(jQuery)
